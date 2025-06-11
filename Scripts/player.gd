9c2375924da281d362c5cc5ea77544882172bc39
Hussain Ahmed <s241058@th-ab.de>
2025-06-11 17:22:50 +0200

    Collisions Fixed

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,52 +1,51 @@
 extends CharacterBody2D
 
-var horizontal_velocity = 0
 var speed = 150  # Movement speed
+var pickup_range = 200
+
+@onready var trash_sprite = get_node("/root/Node2D/TrashContainer/Sprite2D")
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _enter_tree():
 	set_multiplayer_authority(int(str(name)))
 
-@onready var trash_sprite = get_node("/root/Node2D/TrashContainer/Sprite2D")
-@onready var animated_sprite = $AnimatedSprite2D
-var pickup_range = 200
+func _ready():
+	animated_sprite.play()
 
 func pickup():
-	trash_sprite.reparent(self) 
+	trash_sprite.reparent(self)
 	trash_sprite.position = Vector2.ZERO
-	
-		
-func _ready():
-	$AnimatedSprite2D.play()
 
-var screen_size
 func _process(delta):
 	if !is_multiplayer_authority():
 		return
-	var velocity = Vector2.ZERO
+
+	var direction = Vector2.ZERO
+	
+	# Movement Input
 	if Input.is_key_pressed(KEY_D):
-		velocity.x += 1
-		$AnimatedSprite2D.animation = "walk_right"
+		direction.x += 1
+		animated_sprite.animation = "walk_right"
 	if Input.is_key_pressed(KEY_A):
-		velocity.x -= 1
+		direction.x -= 1
 	if Input.is_key_pressed(KEY_W):
-		velocity.y -= 1
+		direction.y -= 1
 	if Input.is_key_pressed(KEY_S):
-		velocity.y += 1
+		direction.y += 1
+	
+	# Pickup Input
 	if Input.is_key_pressed(KEY_E):
 		var dist = global_position.distance_to(trash_sprite.global_position)
 		if dist < pickup_range:
 			pickup()
-	if velocity == Vector2.ZERO:
-		$AnimatedSprite2D.animation = "idle"
-		
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
+
+	# Animation & Movement
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+		animated_sprite.play()
 	else:
-		$AnimatedSprite2D.stop()
-		
-	position += velocity * delta
-
-
-
-		
+		animated_sprite.animation = "idle"
+		animated_sprite.stop()
+	
+	velocity = direction * speed
+	move_and_slide()
