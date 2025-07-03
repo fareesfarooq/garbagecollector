b0a98c2b81780a5b7fc0f16abf97dcd0351b264f
Farees Farooq Ismail <s240199@th-ab.de>
2025-07-03 03:55:47 +0200

    Final Touches

--- menu.gd
+++ menu.gd
@@ -4,7 +4,7 @@ func _on_single_player_pressed() :
 	get_tree().change_scene_to_file("res://main.tscn")
 
 func _on_multi_player_pressed():
-	get_tree().change_scene_to_file("res://main.tscn")
+	get_tree().change_scene_to_file("res://Tutorial.tscn")
 
 
 func _on_options_pressed():
